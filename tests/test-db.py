5c2c1f494149c73fa4efe1ad3173e4d172f4ff59
Chris Down <chris@chrisdown.name>
2013-05-09 20:43:41 +0800

    Add multiple item timelines

--- tests/test-db.py
+++ tests/test-db.py
@@ -4,12 +4,29 @@ import osmo.db
 import time
 
 d = osmo.db.Database(test=True)
-item = {
-    "name": "foo",
-    "start": time.time() - 1,
-    "end": time.time() + 3600,
-    "length": 5,
-    "priority": 1,
+now = time.time()
+items = {
+    "current":{
+        "name": "foo",
+        "start": now - 1,
+        "end": now + 3600,
+        "length": 5,
+        "priority": 1,
+    },
+    "past":{
+        "name": "foo",
+        "start": now - 2,
+        "end": now - 1,
+        "length": 5,
+        "priority": 1,
+    },
+    "future":{
+        "name": "foo",
+        "start": now + 3600,
+        "end": now + 7200,
+        "length": 5,
+        "priority": 1,
+    },
 }
 
 class TestDatabase(object):
@@ -17,15 +34,15 @@ class TestDatabase(object):
         d.r.flushdb()
 
     def testAdd(self):
-        assert all(d.add(**item))
+        assert all(d.add(**items["current"]))
 
     def testAddTwice(self):
-        assert all(d.add(**item))
-        assert not any(d.add(**item))
+        assert all(d.add(**items["current"]))
+        assert not any(d.add(**items["current"]))
 
     def testRem(self):
-        assert all(d.add(**item))
-        assert all(d.rem(item["name"]))
+        assert all(d.add(**items["current"]))
+        assert all(d.rem(items["current"]["name"]))
 
     def testRemVoid(self):
-        assert not any(d.rem(item["name"]))
+        assert not any(d.rem(items["current"]["name"]))
