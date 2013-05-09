225c60466cb7c3becb58bcd44ab8affc2ae49561
Chris Down <chris@chrisdown.name>
2013-05-09 14:54:28 +0800

    Add/remove commands for media

--- osmo/fs.py
+++ osmo/fs.py
@@ -2,18 +2,20 @@
 
 import os
 import osmo.db
+import shutil
 
 class Filesystem(object):
     def __init__(self, root="/srv/osmo", test=False):
         self.root = root
         self.d = { k: os.path.join(self.root, v) for k, v in {
             "media": "media",
-            "now": "now",
         }.items() }
 
     def _cdir(self, name, path):
         return os.path.join(self.d[name], path)
 
-    def link_current(self):
-        for file_name in self.db.media_current():
-            os.symlink(self._cdir("media", file_name), self._cdir("now"))
+    def add(self, path):
+        shutil.copy(path, self._cdir("media"))
+
+    def rem(self, file_name):
+        os.remove(_cdir("media", file_name))
