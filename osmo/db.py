4258efcaaa1e41a24010e3f877e99ce219897aee
Chris Down <chris@chrisdown.name>
2013-05-09 13:57:25 +0800

    Add method to remove media metadata.

--- osmo/db.py
+++ osmo/db.py
@@ -33,6 +33,13 @@ class Database(object):
         p.zadd(self.rk["length"], name, length)
         return p.execute()
 
+    def rem(self, name):
+        p = self.r.pipeline()
+        p.zrem(self.rk["start"],  name)
+        p.zrem(self.rk["end"],    name)
+        p.zrem(self.rk["length"], name)
+        return p.execute()
+
     def media_current(self):
         now = int(time.time())
         started =  set(self.r.zrangebyscore(self.rk["start"], "-inf", now))
