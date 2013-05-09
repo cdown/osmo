d9faef55549c42cc3491ea9b566d52a69111fac0
Chris Down <chris@chrisdown.name>
2013-05-09 13:31:12 +0800

    Add Redis information to __init__

--- osmo/db.py
+++ osmo/db.py
@@ -2,12 +2,22 @@
 
 import json
 import os
+import redis
 
 class Database(object):
-    def __init__(self, root="/srv/osmo"):
+    def __init__(self, root="/srv/osmo", test=False):
+        # Filesystem
         self.root = root
         self.d = { k: os.path.join(self.root, v) for k, v in {
             "media":"media",
             "info":"info",
-            "current":"current",
         }.items() }
+
+        # Redis
+        self.r = redis.Redis()
+        self.keyspace = "osmo"
+        self.rk = {
+            "start":  "%s:start"  % self.keyspace,
+            "end":    "%s:end"    % self.keyspace,
+            "length": "%s:length" % self.keyspace,
+        }
