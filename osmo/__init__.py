d4837d169216044969c136455824c581621844a3
Chris Down <chris@chrisdown.name>
2013-05-09 10:24:56 +0800

    Start writing tests.

---
+++ osmo/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+
+from .db import Database
---
+++ tests/test-db.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import osmo
+import tempfile
+
+tmpDir = tempfile.mkdtemp(prefix="osmo-nose")
+d = osmo.Database(root=tmpDir)
