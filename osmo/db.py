9bed73f122aa3b19a6435e840a1161d9d21b3e15
Chris Down <chris@chrisdown.name>
2013-05-19 16:42:33 +0800

    Improve logging and return structure

--- osmo/db.py
+++ osmo/db.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
+import logging
 import redis
 import time
 
+l = logging.getLogger(__name__)
+
 class Database(object):
     def __init__(self, test=False):
+        l.debug("Initialising connection to Redis database.")
+        l.debug("Database in test mode? %r" % test)
         self.r = redis.Redis(db=1 if test else 0, decode_responses=True)
         self.keyspace = "osmo"
         self.rk = {
@@ -16,15 +21,17 @@ class Database(object):
         }
 
     def add(self, name, start, end, span, rank):
+        l.debug("ADD: %s (%d %d %d %d)" % (name, start, end, span, rank))
         p = self.r.pipeline()
         p.sadd(self.rk["items"], name)
         p.zadd(self.rk["start"], name, start)
         p.zadd(self.rk["end"],   name, end)
         p.zadd(self.rk["span"],  name, span)
         p.zadd(self.rk["rank"],  name, rank)
-        return all(p.execute())
+        return p.execute()
 
     def get(self, name):
+        l.debug("GET: %s" % name)
         p = self.r.pipeline()
         p.zscore(self.rk["start"], name)
         p.zscore(self.rk["end"],   name)
@@ -40,27 +47,43 @@ class Database(object):
         }
 
     def rem(self, name):
+        l.debug("REM: %s" % name)
         p = self.r.pipeline()
         p.srem(self.rk["items"], name)
         p.zrem(self.rk["start"], name)
         p.zrem(self.rk["end"],   name)
         p.zrem(self.rk["span"],  name)
         p.zrem(self.rk["rank"],  name)
-        return all(p.execute())
+        return p.execute()
 
     def current(self):
+        l.debug("Getting current items")
+
         now = int(time.time())
+        l.debug("Epoch time: %d" % now)
+
         p = self.r.pipeline()
         p.zrangebyscore(self.rk["start"], "-inf", now)
         p.zrangebyscore(self.rk["end"], now, "+inf")
-        current_items = set.intersection(*map(set, p.execute()))
+
+        started, not_ended = map(set, p.execute())
+        l.debug("Started: %s" % ", ".join(started))
+        l.debug("Not ended: %s" % ", ".join(not_ended))
+
+        current_items = set.intersection(started, not_ended)
+        l.debug("Intersection: %s" % ", ".join(current_items))
+
         return sorted(
             current_items,
             key=lambda name: self.rank(name)
         )
 
     def rank(self, name):
-        return self.r.zscore(self.rk["rank"], name)
+        rank = self.r.zscore(self.rk["rank"], name)
+        l.debug("Rank of %s: %d" % (name, rank))
+        return rank
 
     def span(self, name):
-        return self.r.zscore(self.rk["span"], name)
+        span = self.r.zscore(self.rk["span"], name)
+        l.debug("Span of %s: %d" % (name, span))
+        return span
--- tests/test-db.py
+++ tests/test-db.py
@@ -37,15 +37,15 @@ class TestDatabase(object):
         d.r.flushdb()
 
     def test_add(self):
-        assert d.add(**items["current"]), "Unable to add item"
+        assert all(d.add(**items["current"])), "Unable to add item"
 
     def test_add_twice(self):
-        assert d.add(**items["current"]), "Unable to add item"
-        assert not d.add(**items["current"]), "Succeeded in readding already present item"
+        assert all(d.add(**items["current"])), "Unable to add item"
+        assert not any(d.add(**items["current"])), "Succeeded in readding already present item"
 
     def test_current(self):
         for period in ("current", "past", "future"):
-            assert d.add(**items[period]), "Unable to add item: %s" % period
+            assert all(d.add(**items[period])), "Unable to add item: %s" % period
 
         current_items = d.current()
         assert len(current_items) == 1, "Expected only 1 current item, but got %d" % len(current_items)
@@ -53,27 +53,27 @@ class TestDatabase(object):
 
     def test_set_get_same(self):
         inItem = items["current"]
-        assert d.add(**inItem), "Unable to add item"
+        assert all(d.add(**inItem)), "Unable to add item"
         outItem = d.get(inItem["name"])
         assert inItem == outItem, """Put in "%r", but got "%r" instead""" % (initem, outItem)
 
     def test_span(self):
         inItem = items["current"]
         inSpan = inItem["span"]
-        assert d.add(**inItem), "Unable to add item"
+        assert all(d.add(**inItem)), "Unable to add item"
         outSpan = d.span(inItem["name"])
         assert inSpan == outSpan, """Put in span "%d", but got "%d" instead""" % (inSpan, outSpan)
 
     def test_rank(self):
         inItem = items["current"]
         inRank = inItem["rank"]
-        assert d.add(**inItem), "Unable to add item"
+        assert all(d.add(**inItem)), "Unable to add item"
         outRank = d.rank(inItem["name"])
         assert inRank == outRank, """Put in rank "%d", but got "%d" instead""" % (inRank, outRank)
 
     def test_rem(self):
-        assert d.add(**items["current"]), "Unable to add item"
-        assert d.rem(items["current"]["name"]), "Unable to remove item"
+        assert all(d.add(**items["current"])), "Unable to add item"
+        assert all(d.rem(items["current"]["name"])), "Unable to remove item"
 
     def test_rem_nonexistent(self):
-        assert not d.rem(items["current"]["name"]), "Removed nonexistent item"
+        assert not any(d.rem(items["current"]["name"])), "Removed nonexistent item"
