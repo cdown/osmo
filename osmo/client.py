100c8aa0a5f2f0df42120e08c684139a198b138c
Chris Down <chris@chrisdown.name>
2013-06-26 11:28:29 +0800

    Migrate client HTML/CSS/JS to template

--- osmo/client.py
+++ osmo/client.py
@@ -19,53 +19,7 @@ def stream():
 
 @app.route("/", methods=[ "GET" ])
 def home():
-    return """
-<!doctype html>
-<style type="text/css">
-    body {
-        background-attachment: fixed;
-        background-position: 50% 50%;
-        background-repeat: no-repeat no-repeat;
-    }
-
-    .constrainHeight {
-        background-size: auto 100%;
-    }
-
-    .constrainWidth {
-        background-size: 100% auto;
-    }
-</style>
-<script type="text/javascript" src="http://code.jquery.com/jquery-2.0.0.min.js"></script>
-<script>
-    function scale_bg(url) {
-        var img = new Image;
-        img.src = url;
-        var imageAspectRatio = img.width / img.height;
-        var backgroundAspectRatio = $(window).width() / $(window).height();
-
-        if (imageAspectRatio < backgroundAspectRatio) {
-            $("body")
-                .removeClass()
-                .addClass('constrainHeight');
-        } else {
-            $("body")
-                .removeClass()
-                .addClass('constrainWidth');
-        }
-    }
-    function sse() {
-        var stream = new EventSource("/stream");
-        stream.onmessage = function(e) {
-            $("body").css("background-image",
-                "url(/media/" + e.data + ")"
-            );
-            scale_bg("/media/" + e.data);
-        };
-    }
-    sse();
-</script>
-"""
+    return flask.render_template("client.html")
 
 @app.route("/media/<path:name>", methods=[ "GET" ])
 def media(name):
