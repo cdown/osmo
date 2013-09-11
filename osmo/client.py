0f236c489226fb2bf3e97c12cdd02dea1e0cc69e
Chris Down <chris@chrisdown.name>
2013-09-11 03:26:38 +0200

    Client PEP8 compliance

--- osmo/client.py
+++ osmo/client.py
@@ -9,13 +9,19 @@ import flask
 import redis
 import argparse
 
+
 app = flask.Flask(__name__)
 r = redis.Redis()
 
 # This is only eventually used when we are not being run in the Flask debugger.
 media_dir = "/srv/osmo"
 
+
 def event_stream():
+    """
+    Generate data to populate the client SSE stream.
+    """
+
     ps = r.pubsub()
     ps.subscribe("osmo")
     for message in ps.listen():
@@ -23,23 +29,39 @@ def event_stream():
             data = message["data"].decode("utf8")
         except AttributeError:
             data = message["data"]
+        finally:
+            yield "data: %s\n\n" % data
 
-        yield "data: %s\n\n" % data
 
-@app.route("/stream", methods=[ "GET" ])
+@app.route("/stream", methods=["GET"])
 def stream():
+    """
+    Provides the slide SSE stream.
+    """
+
     return flask.Response(
         event_stream(),
         mimetype="text/event-stream"
     )
 
-@app.route("/", methods=[ "GET" ])
+
+@app.route("/", methods=["GET"])
 def home():
+    """
+    Provides the framework to display images based on the SSE stream.
+    """
+
     return flask.render_template("client.html")
 
-@app.route("/media/<path:name>", methods=[ "GET" ])
+
+@app.route("/media/<path:name>", methods=["GET"])
 def media(name):
+    """
+    Get a file from the media directory.
+    """
+
     return flask.send_from_directory(media_dir, name)
+
 
 if __name__ == "__main__":
     import argparse
