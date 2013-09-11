b64a97aa2dd60b3420b62bfa0eaf5e86a1684acd
Chris Down <chris@chrisdown.name>
2013-09-11 03:38:27 +0200

    Simplify publisher

--- osmo/publisher.py
+++ osmo/publisher.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
+"""
+Publish the currently active slides to Redis.
+"""
+
 import db
 import redis
 import time
 import sys
+
 
 if len(sys.argv) > 1 and sys.argv[1] == "--test":
     test = True
@@ -13,16 +18,15 @@ else:
 r = redis.Redis(port=6379 if not test else 28692)
 d = db.Database(test=test)
 
-if __name__ == "__main__":
-    while True:
-        active = d.get_state(state="active")
-        if not active:
-            print("No active items, sleeping for 5 seconds")
-            r.publish("osmo", "__empty__")
-            time.sleep(5)
-            continue
+while True:
+    active = d.get_state(state="active")
+    if not active:
+        print("No active items, sleeping for 5 seconds")
+        r.publish("osmo", "__empty__")
+        time.sleep(5)
+        continue
 
-        for name, duration in active:
-            print("Publishing %s, sleeping for %d seconds" % (name, duration))
-            r.publish("osmo", name)
-            time.sleep(duration)
+    for name, duration in active:
+        print("Publishing %s, sleeping for %d seconds" % (name, duration))
+        r.publish("osmo", name)
+        time.sleep(duration)
