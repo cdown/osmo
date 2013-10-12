beb28bb5085af080675a8d24b59925b4225a4985
Chris Down <chris@chrisdown.name>
2013-10-12 14:48:57 +0800

    Use docopt to parse args in publisher.

--- osmo/publisher.py
+++ osmo/publisher.py
@@ -2,31 +2,38 @@
 
 """
 Publish the currently active slides to Redis.
+
+Usage: publisher.py [options]
+
+Options:
+  --help           This help text.
+  --test           Use the test Redis database.
+  --port PORT      The port to use. [default: 6379]
+  --sleep SECONDS  The sleep time when there are no active slides. [default: 5]
 """
 
+from docopt import docopt
 import db
 import redis
-import time
 import sys
+import time
 
 
-if len(sys.argv) > 1 and sys.argv[1] == "--test":
-    test = True
-else:
-    test = False
+if __name__ == "__main__":
+    args = docopt(__doc__)
 
-r = redis.Redis(port=6379 if not test else 28692)
-d = db.Database(test=test)
+    r = redis.Redis(port=int(args["--port"]))
+    d = db.Database(test=args["--test"])
 
-while True:
-    active = d.slides_in_state("active")
-    if not active:
-        print("No active slides, sleeping for 5 seconds")
-        r.publish("osmo", "__empty__")
-        time.sleep(5)
-        continue
+    while True:
+        active = d.slides_in_state("active")
 
-    for name, slide in active:
-        print("Publishing %s, waiting %d seconds" % (name, slide["duration"]))
-        r.publish("osmo", name)
-        time.sleep(slide["duration"])
+        if active:
+            for name, slide in active:
+                print("Published %s, %d seconds" % (name, slide["duration"]))
+                r.publish("osmo", name)
+                time.sleep(slide["duration"])
+        else:
+            print("No active slides, sleeping %s seconds" % args["--sleep"])
+            r.publish("osmo", "__empty__")
+            time.sleep(int(args["--sleep"]))
