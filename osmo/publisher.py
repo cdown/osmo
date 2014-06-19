9e0be7502b192c93f854599d027ca236e093a118
Ward Vandewege <ward@gnu.org>
2014-06-20 00:21:54 +0200

    Change publisher to use sys.stdout for its output, and make sure to flush() it.
    This makes it usable with runit, which is a much nicer way to run this kind of
    program.

--- osmo/publisher.py
+++ osmo/publisher.py
@@ -16,11 +16,13 @@ if __name__ == "__main__":
 
         if active:
             for name, slide in active:
-                print("Published %s, %d seconds" % (name, slide["duration"]))
+                sys.stdout.write("Published %s, %d seconds\n" % (name, slide["duration"]))
+                sys.stdout.flush()
                 r.publish("osmo", name)
                 time.sleep(slide["duration"])
         else:
             sleep_time = config["publisher"]["empty_sleep"]
-            print("No active slides, sleeping %d seconds" % sleep_time)
+            sys.stdout.write("No active slides, sleeping %d seconds\n" % sleep_time)
+            sys.stdout.flush()
             r.publish("osmo", "__empty__")
             time.sleep(sleep_time)
