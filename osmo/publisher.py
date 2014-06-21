5fc65d5daf283142ff986e16aeceb1afd57010ef
Chris Down <chris@chrisdown.name>
2014-06-21 10:56:09 +0800

    publisher: Use `print' when printing slide information

--- osmo/publisher.py
+++ osmo/publisher.py
@@ -16,13 +16,13 @@ if __name__ == "__main__":
 
         if active:
             for name, slide in active:
-                sys.stdout.write("Published %s, %d seconds\n" % (name, slide["duration"]))
+                print("Published %s, %d seconds\n" % (name, slide["duration"]))
                 sys.stdout.flush()
                 r.publish("osmo", name)
                 time.sleep(slide["duration"])
         else:
             sleep_time = config["publisher"]["empty_sleep"]
-            sys.stdout.write("No active slides, sleeping %d seconds\n" % sleep_time)
+            print("No active slides, sleeping %d seconds\n" % sleep_time)
             sys.stdout.flush()
             r.publish("osmo", "__empty__")
             time.sleep(sleep_time)
