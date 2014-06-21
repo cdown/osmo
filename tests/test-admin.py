6baf409d1f7a4e906d1ff938fa1cc14c77025bf9
Chris Down <chris@chrisdown.name>
2014-06-21 12:18:21 +0800

    tests: Test all extensions when testing _allowed_file()

--- tests/test-admin.py
+++ tests/test-admin.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 
 import osmo.admin
+from osmo.config import config
 
 
-def test_allowed_file():
-    assert osmo.admin._allowed_file("foo.bar") == False
-    assert osmo.admin._allowed_file("foo.jpg") == True
+def test_allowed_file_bad():
+    assert not any(
+        osmo.admin._allowed_file("foo.%sx" % ext)
+        for ext in config["admin"]["valid_extensions"]
+    )
+
+
+def test_allowed_file_good():
+    assert all(
+        osmo.admin._allowed_file("foo.%s" % ext)
+        for ext in config["admin"]["valid_extensions"]
+    )
